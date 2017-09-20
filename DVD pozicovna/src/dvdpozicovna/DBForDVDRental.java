4304b851a4f8503c437788f4045c75a82f74cfbf
Imrichkoch <imrichkoch@gmail.com>
2017-09-20 09:49:29 +0200

    .

--- DVD pozicovna/src/dvdpozicovna/DBForDVDRental.java
+++ DVD pozicovna/src/dvdpozicovna/DBForDVDRental.java
@@ -20,26 +20,26 @@ public class DBForDVDRental {
 			conn = DriverManager.getConnection(DB_URL, USER, PWD);
 			stmt = conn.createStatement();
 
-			String sql = "CREATE TYPE MovieCategory AS ENUM ('Comedy', 'Action', 'Horror','Sci-Fi',"
+			String sql = "CREATE TYPE dvd.MovieCategory AS ENUM" + " ('Comedy', 'Action', 'Horror','Sci-Fi',"
 					+ " 'Drama', 'Musical','Sport', 'Documentary', 'Adult')";
 			stmt.executeUpdate(sql);
 
-			sql = "CREATE TYPE PrizeCategory AS ENUM ('A', 'B', 'C')";
+			sql = "CREATE TYPE dvd.PrizeCategory AS ENUM ('A', 'B', 'C')";
 			stmt.executeUpdate(sql);
 
-			sql = "CREATE TABLE dvd.DVD (id integer(10) not null, rental_id integer(10), name varchar,"
-					+ "genre MovieCategory, prize_category PrizeCategory, count integer(3),"
-					+ "rented_count integer(3), primary key (id))";
+			sql = "CREATE TABLE dvd.User (id numeric(10) not null, " + "first_name varchar, " + "last_name varchar,"
+					+ "address_street varchar, " + "address_city varchar, " + "address_zip varchar," + "email varchar,"
+					+ "tel_number varchar, " + "primary key (id))";
 			stmt.executeUpdate(sql);
 
-			sql = "CREATE TABLE dvd.Rental (id integer(10) not null, user_id integer(10),"
-					+ "date_from Date, date_to Date," + " primary key (id))";
+			sql = "CREATE TABLE dvd.Rental (id numeric(10) not null, " + "user_id numeric(10) REFERENCES dvd.User(id),"
+					+ "date_from Date, " + "date_to Date," + "primary key (id))";
 			stmt.executeUpdate(sql);
 
-			sql = "ALTER TABLE dvd.Rental  ADD FOREIGN KEY (user_id) REFERENCES dvd.DVD (rental_id)";
-			stmt.executeUpdate(sql);
-
-			sql = "CREATE TABLE dvd.User (id integer(10) not null, first_name varchar, last_name varchar,address_street varchar, address_city varchar, address_zip varchar,email varchar,tel_number varchar, primary key (id))";
+			sql = "CREATE TABLE dvd.DVD (id numeric(10) not null, "
+					+ "rental_id numeric(10) REFERENCES dvd.Rental(id), " + "name varchar," + "genre dvd.MovieCategory,"
+					+ "prize_category dvd.PrizeCategory," + "count numeric(3)," + "rented_count numeric(3),"
+					+ "primary key (id))";
 			stmt.executeUpdate(sql);
 
 		} catch (ClassNotFoundException e) {
